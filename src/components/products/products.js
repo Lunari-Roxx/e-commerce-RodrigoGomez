a04072fcf58aa2fdf46f0a1bbb91d14e64952180
Lunari-Roxx <rorogs25@gmail.com>
2022-02-02 18:12:42 -0300

    delete temporal del componente products, los datos los productos los pongo a mano en la misma promesa

--- src/components/products/products.js
+++
@@ -1,8 +0,0 @@
-
-
-export const prod = [
-    {id:1 , title : "Monster energy", price: 90 , pictureURL:""},
-    {id:2 , title : "RedBull", price: 120 , pictureURL:""},
-    {id:3 , title : "Rockstar", price: 100 , pictureURL:""},
-    {id:4 , title : "Zevia", price: 150 , pictureURL:""}
-];
